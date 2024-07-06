8b92bd1d00f69eddd7670086508985ac885791ca
Raj Aryan Singh <rajlysm15@gmail.com>
2024-07-07 02:14:35 +0530

    Add basic game management: Game struct and join functionality

--- backend/main.go
+++ backend/main.go
@@ -1,22 +1,84 @@
+// main.go
 package main
 
 import (
     "log"
     "net/http"
+    "sync"
     "golang.org/x/net/websocket"
 )
 
+type Game struct {
+    Players map[string]*websocket.Conn
+    mutex   sync.Mutex
+}
+
+var games = make(map[string]*Game)
+var gamesMutex sync.Mutex
+
 func handleWebSocket(ws *websocket.Conn) {
     log.Println("New connection from:", ws.Request().RemoteAddr)
+
+    var playerID string
+    var roomID string
+
     for {
-        var message string
-        err := websocket.Message.Receive(ws, &message)
+        var msg struct {
+            Type    string `json:"type"`
+            RoomID  string `json:"roomId"`
+            PlayerID string `json:"playerId"`
+        }
+        err := websocket.JSON.Receive(ws, &msg)
         if err != nil {
             log.Println("Error receiving message:", err)
             break
         }
-        log.Println("Received message:", message)
-        websocket.Message.Send(ws, message)
+
+        switch msg.Type {
+        case "join":
+            roomID = msg.RoomID
+            playerID = msg.PlayerID
+            joinGame(roomID, playerID, ws)
+        default:
+            log.Println("Unknown message type:", msg.Type)
+        }
+    }
+
+    if roomID != "" {
+        leaveGame(roomID, playerID)
+    }
+}
+
+func joinGame(roomID, playerID string, ws *websocket.Conn) {
+    gamesMutex.Lock()
+    defer gamesMutex.Unlock()
+
+    if game, exists := games[roomID]; exists {
+        game.mutex.Lock()
+        defer game.mutex.Unlock()
+        game.Players[playerID] = ws
+    } else {
+        games[roomID] = &Game{
+            Players: map[string]*websocket.Conn{playerID: ws},
+        }
+    }
+}
+
+func leaveGame(roomID, playerID string) {
+    gamesMutex.Lock()
+    defer gamesMutex.Unlock()
+
+    game, exists := games[roomID]
+    if !exists {
+        return
+    }
+
+    game.mutex.Lock()
+    defer game.mutex.Unlock()
+
+    delete(game.Players, playerID)
+    if len(game.Players) == 0 {
+        delete(games, roomID)
     }
 }
 
